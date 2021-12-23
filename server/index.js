b54985e91ac68772f12901d9a26c8c6d6a80ae07
Sumith Deepan <sumith.d@falainacloud.com>
2021-12-23 17:57:05 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,15 @@
+import express from "express";
+import BodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
+
+const app = express()
+
+app.use(BodyParser.json({ limit: "30mb", extended: true }));
+app.use(BodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL = "mongodb+srv://kaytech:Kaytech123@cluster0.9ktxi.mongodb.net/KayTechDB?retryWrites=true&w=majority"
+const PORT = process.env.PORT || 5000
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => app.listen(PORT, () => console.log(`Server running on port : ${PORT}`))).catch((err) => console.log(err.messages))
