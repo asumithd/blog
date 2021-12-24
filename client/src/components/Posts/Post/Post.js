3ba246d79ab7d19ba7cc339bd1071f859209d377
Sumith Deepan <sumith.d@falainacloud.com>
2021-12-24 14:35:04 +0530

    post update

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,12 +1,24 @@
-import React from 'react'
-import useStyles from './Styles'
-const Post = () => {
-    const classes = useStyles()
-    return (
-        <>
-            <h1>Post</h1>
-        </>
-    )
-}
+import { Card, CardMedia, Typography } from "@material-ui/core";
+import React from "react";
+import moment from "moment";
+import useStyles from "./Styles";
+const Post = ({ post }) => {
+  const classes = useStyles();
+  console.log(post);
+  
+  return (
+    <Card className={classes.card}>
+      <CardMedia
+        className={classes.media}
+        image={postMessage.selectedFile}
+        file={post.title}
+      />
+      <div className={classes.overlay}>
+        <Typography variant="h6">{post.creator}</Typography>
+        <Typography variant="body2">{moment(post.creatAt).fromNow()}</Typography>
+      </div>
+    </Card>
+  );
+};
 
-export default Post
+export default Post;
